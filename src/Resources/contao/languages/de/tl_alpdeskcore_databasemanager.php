8d54db7e8493216c72e8f22b6d2bf901522c80ff
XProjects B.Hummel <infos@x-projects.de>
2021-03-08 22:29:26 +0100

    First version of migration

--- src/Resources/contao/languages/de/tl_alpdeskcore_databasemanager.php
+++ src/Resources/contao/languages/de/tl_alpdeskcore_databasemanager.php
@@ -6,6 +6,7 @@ $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['port'] = array('Port', 'P
 $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['database'] = array('Datenbank', 'Name der Datenbank');
 $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['username'] = array('Benutzer', 'Benutzer der Datenbankverbindung');
 $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['password'] = array('Password', 'Password der Datenbankverbindung');
+$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['databasemodel'] = array('Datenbankmodel (*.json)', 'Wählen sie ein Datenbankmodel aus');
 $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['dbmigration'] = array('Output', '');
 
 $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['new'] = array('Neue Datanbankverbindung', 'Neue Datanbankverbindung hinzufügen');
@@ -17,3 +18,8 @@ $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['valid_parameters'] = 'Par
 $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['invalid_parameters'] = 'Parameter ungültig';
 $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['valid_connection'] = 'Verbindung ok';
 $GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['invalid_connection'] = 'ungültige Verbindung';
+$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['configurationcheck_valid'] = 'Datenbank-Check in Ordnung';
+$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['migrations'] = 'Datenbank-Migrationen';
+$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['nomigrations'] = 'Datenbank ist aktuell';
+$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['migratelink'] = 'Migration durchführen';
+$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['migrateconfirm'] = 'Migration wirklich durchführen?';
