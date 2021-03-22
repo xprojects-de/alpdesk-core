7cf9b1ca235d27120911b48e4627b38135a82367
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 16:11:04 +0100

    code refactoring

--- src/Resources/contao/dca/tl_alpdeskcore_databasemanager.php
+++ src/Resources/contao/dca/tl_alpdeskcore_databasemanager.php
@@ -1,43 +1,43 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_alpdeskcore_databasemanager'] = array
-    (
+(
     'config' => array
-        (
+    (
         'dataContainer' => 'Table',
         'enableVersioning' => true,
         'sql' => array
-            (
+        (
             'keys' => array
-                (
+            (
                 'id' => 'primary'
             )
         )
     ),
     'list' => array
-        (
+    (
         'sorting' => array
-            (
+        (
             'mode' => 2,
             'fields' => array('title ASC'),
             'flag' => 1,
             'panelLayout' => 'sort,search,limit'
         ),
         'label' => array
-            (
+        (
             'fields' => array('title', 'host', 'database'),
             'showColumns' => true
         ),
         'operations' => array
-            (
+        (
             'edit' => array
-                (
+            (
                 'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['edit'],
                 'href' => 'act=edit',
                 'icon' => 'edit.gif'
             ),
             'delete' => array
-                (
+            (
                 'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.gif',
@@ -46,21 +46,21 @@ $GLOBALS['TL_DCA']['tl_alpdeskcore_databasemanager'] = array
         )
     ),
     'palettes' => array
-        (
+    (
         'default' => 'title;host,port;database,username,password;databasemodel;dbmigration'
     ),
     'fields' => array
-        (
+    (
         'id' => array
-            (
+        (
             'sql' => "int(10) unsigned NOT NULL auto_increment"
         ),
         'tstamp' => array
-            (
+        (
             'sql' => "int(10) unsigned NOT NULL default '0'"
         ),
         'title' => array
-            (
+        (
             'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['title'],
             'exclude' => true,
             'search' => true,
@@ -69,7 +69,7 @@ $GLOBALS['TL_DCA']['tl_alpdeskcore_databasemanager'] = array
             'sql' => "varchar(250) NOT NULL default ''"
         ),
         'host' => array
-            (
+        (
             'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['host'],
             'exclude' => true,
             'search' => true,
@@ -79,7 +79,7 @@ $GLOBALS['TL_DCA']['tl_alpdeskcore_databasemanager'] = array
             'sql' => "varchar(250) NOT NULL default ''"
         ),
         'port' => array
-            (
+        (
             'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['port'],
             'exclude' => true,
             'search' => false,
@@ -89,7 +89,7 @@ $GLOBALS['TL_DCA']['tl_alpdeskcore_databasemanager'] = array
             'sql' => "varchar(250) NOT NULL default ''"
         ),
         'database' => array
-            (
+        (
             'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['database'],
             'exclude' => true,
             'search' => true,
@@ -98,7 +98,7 @@ $GLOBALS['TL_DCA']['tl_alpdeskcore_databasemanager'] = array
             'sql' => "varchar(250) NOT NULL default ''"
         ),
         'username' => array
-            (
+        (
             'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['username'],
             'exclude' => true,
             'search' => false,
@@ -107,24 +107,24 @@ $GLOBALS['TL_DCA']['tl_alpdeskcore_databasemanager'] = array
             'sql' => "varchar(250) NOT NULL default ''"
         ),
         'password' => array
-            (
+        (
             'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['password'],
             'exclude' => true,
             'search' => false,
             'inputType' => 'text',
             'eval' => array('mandatory' => true, 'maxlength' => 250, 'tl_class' => 'w50', 'hideInput' => false),
             'save_callback' => array
-                (
+            (
                 array('Alpdesk\\AlpdeskCore\\Library\\Backend\\AlpdeskCoreDcaUtils', 'generateEncryptPassword')
             ),
             'load_callback' => array
-                (
+            (
                 array('Alpdesk\\AlpdeskCore\\Library\\Backend\\AlpdeskCoreDcaUtils', 'regenerateEncryptPassword')
             ),
             'sql' => "varchar(250) NOT NULL default ''"
         ),
         'databasemodel' => array
-            (
+        (
             'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_databasemanager']['databasemodel'],
             'exclude' => true,
             'inputType' => 'fileTree',
@@ -137,7 +137,7 @@ $GLOBALS['TL_DCA']['tl_alpdeskcore_databasemanager'] = array
             'sql' => "binary(16) NULL"
         ),
         'dbmigration' => array
-            (
+        (
             'exclude' => true,
             'inputType' => 'alpdeskcore_widget_databasemanager',
             'eval' => array('doNotSaveEmpty' => true)
