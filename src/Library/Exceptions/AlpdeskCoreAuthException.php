5b235d4d579814ae96b8f8137f8262f193858815
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 15:34:26 +0100

    code refactoring

--- src/Library/Exceptions/AlpdeskCoreAuthException.php
+++ src/Library/Exceptions/AlpdeskCoreAuthException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Alpdesk\AlpdeskCore\Library\Exceptions;
 
-class AlpdeskCoreAuthException extends \Exception {
-  
+class AlpdeskCoreAuthException extends \Exception
+{
+
 }
--- src/Library/Exceptions/AlpdeskCorePluginException.php
+++ src/Library/Exceptions/AlpdeskCorePluginException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Alpdesk\AlpdeskCore\Library\Exceptions;
 
-class AlpdeskCorePluginException extends \Exception {
-  
+class AlpdeskCorePluginException extends \Exception
+{
+
 }
