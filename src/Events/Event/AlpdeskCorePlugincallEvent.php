dfd9ebe54a4d3479356db942a81bd45ba5b8f77e
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 15:13:43 +0100

    code refactoring

--- src/Events/Event/AlpdeskCorePlugincallEvent.php
+++ src/Events/Event/AlpdeskCorePlugincallEvent.php
@@ -7,22 +7,24 @@ namespace Alpdesk\AlpdeskCore\Events\Event;
 use Symfony\Contracts\EventDispatcher\Event;
 use Alpdesk\AlpdeskCore\Library\Plugin\AlpdeskCorePlugincallResponse;
 
-class AlpdeskCorePlugincallEvent extends Event {
+class AlpdeskCorePlugincallEvent extends Event
+{
+    public const NAME = 'alpdesk.plugincall';
 
-  public const NAME = 'alpdesk.plugincall';
+    private AlpdeskCorePlugincallResponse $resultData;
 
-  private AlpdeskCorePlugincallResponse $resultData;
+    public function __construct(AlpdeskCorePlugincallResponse $resultData)
+    {
+        $this->resultData = $resultData;
+    }
 
-  public function __construct(AlpdeskCorePlugincallResponse $resultData) {
-    $this->resultData = $resultData;
-  }
+    public function getResultData(): AlpdeskCorePlugincallResponse
+    {
+        return $this->resultData;
+    }
 
-  public function getResultData(): AlpdeskCorePlugincallResponse {
-    return $this->resultData;
-  }
-
-  public function setResultData(AlpdeskCorePlugincallResponse $resultData) {
-    $this->resultData = $resultData;
-  }
-
+    public function setResultData(AlpdeskCorePlugincallResponse $resultData)
+    {
+        $this->resultData = $resultData;
+    }
 }
