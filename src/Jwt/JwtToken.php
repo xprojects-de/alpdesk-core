825aebe4178b3667f62fcf87f3591fa7eab5d5c2
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 15:20:08 +0100

    code refactoring

--- src/Jwt/JwtToken.php
+++ src/Jwt/JwtToken.php
@@ -14,93 +14,107 @@ use Lcobucci\JWT\Validation\Constraint\IdentifiedBy;
 use Lcobucci\JWT\Validation\Constraint\SignedWith;
 use Contao\System;
 
-class JwtToken {
+class JwtToken
+{
+    private static string $issuedBy = 'Alpdesk';
+    private static string $permittedFor = 'https://alpdesk.de';
 
-  private static string $issuedBy = 'Alpdesk';
-  private static string $permittedFor = 'https://alpdesk.de';
-
-  private static function getDefaultKeyString(): string {
-    $keyString = System::getContainer()->getParameter('kernel.secret');
-    return substr($keyString, 10, 32);
-  }
-
-  private static function getConfig(): Configuration {
-    return Configuration::forSymmetricSigner(new Sha256(), InMemory::plainText(self::getDefaultKeyString()));
-  }
-
-  public static function generate(string $jti, int $nbf = 3600, array $claims = array()): string {
-
-    $time = time();
-
-    $config = self::getConfig();
-
-    $issuesAt = (new \DateTimeImmutable())->setTimestamp($time);
-    $usedAfter = (new \DateTimeImmutable())->setTimestamp($time + 0);
-    $expiresAt = (new \DateTimeImmutable())->setTimestamp($time + $nbf);
-
-    $builder = $config->builder();
-    $builder->issuedBy(self::$issuedBy); // iss claim
-    $builder->permittedFor(self::$permittedFor); // iss claim
-    $builder->identifiedBy($jti, true); // jti claim
-    $builder->issuedAt($issuesAt); // iat claim
-
-    $builder->canOnlyBeUsedAfter($usedAfter); // Configures the time that the token can be used (nbf claim)
-    if ($nbf > 0) {
-      $builder->expiresAt($expiresAt); // Configures the expiration time of the token (exp claim)
-    }
-    if (count($claims) > 0) {
-      foreach ($claims as $keyClaim => $valueClaim) {
-        $builder->withClaim($keyClaim, $valueClaim);
-      }
-    }
-    $token = $builder->getToken($config->signer(), $config->signingKey());
-    $tokenString = $token->toString();
-    return $tokenString;
-  }
-
-  public static function parse(string $token): Token {
-    $config = self::getConfig();
-    $parser = $config->parser();
-    $tokenObject = $parser->parse((string) $token);
-    return $tokenObject;
-  }
-
-  public static function getClaim(string $token, string $name) {
-    $value = null;
-    try {
-      $tokenObject = self::parse($token);
-      $value = $tokenObject->claims()->get($name);
-    } catch (\Exception $ex) {
-      $value = null;
-    }
-    return $value;
-  }
-
-  public static function validateAndVerify(string $token, string $jti): bool {
-    $tokenObject = self::parse($token);
-
-    $config = self::getConfig();
-
-    $issuedByConstraints = new IssuedBy(self::$issuedBy);
-    $permittedForConstraints = new PermittedFor(self::$permittedFor);
-    $identifiedByConstraints = new IdentifiedBy($jti);
-
-    $validator = $config->validator();
-
-    $value = false;
-    try {
-      $signer = new SignedWith($config->signer(), InMemory::plainText(self::getDefaultKeyString()));
-      $validator->assert($tokenObject, $signer);
-      $value = $validator->validate($tokenObject, $issuedByConstraints, $permittedForConstraints, $identifiedByConstraints);
-      if ($value == true) {
-        $now = (new \DateTimeImmutable())->setTimestamp(time());
-        $value = !$tokenObject->isExpired($now);
-      }
-    } catch (\Exception $ex) {
-      $value = false;
+    private static function getDefaultKeyString(): string
+    {
+        $keyString = System::getContainer()->getParameter('kernel.secret');
+        return substr($keyString, 10, 32);
     }
 
-    return $value;
-  }
+    private static function getConfig(): Configuration
+    {
+        return Configuration::forSymmetricSigner(new Sha256(), InMemory::plainText(self::getDefaultKeyString()));
+    }
 
+    public static function generate(string $jti, int $nbf = 3600, array $claims = array()): string
+    {
+        $time = time();
+
+        $config = self::getConfig();
+
+        $issuesAt = (new \DateTimeImmutable())->setTimestamp($time);
+        $usedAfter = (new \DateTimeImmutable())->setTimestamp($time + 0);
+        $expiresAt = (new \DateTimeImmutable())->setTimestamp($time + $nbf);
+
+        $builder = $config->builder();
+        $builder->issuedBy(self::$issuedBy); // iss claim
+        $builder->permittedFor(self::$permittedFor); // iss claim
+        $builder->identifiedBy($jti); // jti claim
+        $builder->issuedAt($issuesAt); // iat claim
+
+        $builder->canOnlyBeUsedAfter($usedAfter); // Configures the time that the token can be used (nbf claim)
+        if ($nbf > 0) {
+            $builder->expiresAt($expiresAt); // Configures the expiration time of the token (exp claim)
+        }
+
+        if (\count($claims) > 0) {
+            foreach ($claims as $keyClaim => $valueClaim) {
+                $builder->withClaim($keyClaim, $valueClaim);
+            }
+        }
+
+        $token = $builder->getToken($config->signer(), $config->signingKey());
+
+        return $token->toString();
+    }
+
+    public static function parse(string $token): Token
+    {
+        $config = self::getConfig();
+        $parser = $config->parser();
+
+        return $parser->parse((string)$token);
+    }
+
+    public static function getClaim(string $token, string $name)
+    {
+        $value = null;
+
+        try {
+
+            $tokenObject = self::parse($token);
+            $value = $tokenObject->claims()->get($name);
+
+        } catch (\Exception $ex) {
+            $value = null;
+        }
+
+        return $value;
+    }
+
+    public static function validateAndVerify(string $token, string $jti): bool
+    {
+        $tokenObject = self::parse($token);
+
+        $config = self::getConfig();
+
+        $issuedByConstraints = new IssuedBy(self::$issuedBy);
+        $permittedForConstraints = new PermittedFor(self::$permittedFor);
+        $identifiedByConstraints = new IdentifiedBy($jti);
+
+        $validator = $config->validator();
+
+        $value = false;
+
+        try {
+
+            $signer = new SignedWith($config->signer(), InMemory::plainText(self::getDefaultKeyString()));
+            $validator->assert($tokenObject, $signer);
+            $value = $validator->validate($tokenObject, $issuedByConstraints, $permittedForConstraints, $identifiedByConstraints);
+
+            if ($value == true) {
+                $now = (new \DateTimeImmutable())->setTimestamp(time());
+                $value = !$tokenObject->isExpired($now);
+            }
+
+        } catch (\Exception $ex) {
+            $value = false;
+        }
+
+        return $value;
+    }
 }
