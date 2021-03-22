e93b2481f199478c6d05ad5e86cd9a0bfd58cd0b
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 15:07:33 +0100

    code refactoring

--- src/Events/Event/AlpdeskCoreAuthInvalidEvent.php
+++ src/Events/Event/AlpdeskCoreAuthInvalidEvent.php
@@ -7,22 +7,24 @@ namespace Alpdesk\AlpdeskCore\Events\Event;
 use Symfony\Contracts\EventDispatcher\Event;
 use Alpdesk\AlpdeskCore\Library\Auth\AlpdeskCoreAuthResponse;
 
-class AlpdeskCoreAuthInvalidEvent extends Event {
+class AlpdeskCoreAuthInvalidEvent extends Event
+{
+    public const NAME = 'alpdesk.auth_invalid';
 
-  public const NAME = 'alpdesk.auth_invalid';
+    private AlpdeskCoreAuthResponse $resultData;
 
-  private AlpdeskCoreAuthResponse $resultData;
+    public function __construct(AlpdeskCoreAuthResponse $resultData)
+    {
+        $this->resultData = $resultData;
+    }
 
-  public function __construct(AlpdeskCoreAuthResponse $resultData) {
-    $this->resultData = $resultData;
-  }
+    public function getResultData(): AlpdeskCoreAuthResponse
+    {
+        return $this->resultData;
+    }
 
-  public function getResultData(): AlpdeskCoreAuthResponse {
-    return $this->resultData;
-  }
-
-  public function setResultData(AlpdeskCoreAuthResponse $resultData) {
-    $this->resultData = $resultData;
-  }
-
+    public function setResultData(AlpdeskCoreAuthResponse $resultData)
+    {
+        $this->resultData = $resultData;
+    }
 }
