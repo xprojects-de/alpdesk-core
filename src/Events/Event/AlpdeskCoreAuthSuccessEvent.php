f115b8b697a2a0bd2f014163b0baf929dab8d76e
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 15:10:26 +0100

    code refactoring

--- src/Events/Event/AlpdeskCoreAuthSuccessEvent.php
+++ src/Events/Event/AlpdeskCoreAuthSuccessEvent.php
@@ -7,22 +7,24 @@ namespace Alpdesk\AlpdeskCore\Events\Event;
 use Symfony\Contracts\EventDispatcher\Event;
 use Alpdesk\AlpdeskCore\Library\Auth\AlpdeskCoreAuthResponse;
 
-class AlpdeskCoreAuthSuccessEvent extends Event {
+class AlpdeskCoreAuthSuccessEvent extends Event
+{
+    public const NAME = 'alpdesk.auth_success';
 
-  public const NAME = 'alpdesk.auth_success';
+    private AlpdeskCoreAuthResponse $resultData;
 
-  private AlpdeskCoreAuthResponse $resultData;
+    public function __construct(AlpdeskCoreAuthResponse $resultData)
+    {
+        $this->resultData = $resultData;
+    }
 
-  public function __construct(AlpdeskCoreAuthResponse $resultData) {
-    $this->resultData = $resultData;
-  }
+    public function getResultData(): AlpdeskCoreAuthResponse
+    {
+        return $this->resultData;
+    }
 
-  public function getResultData(): AlpdeskCoreAuthResponse {
-    return $this->resultData;
-  }
-
-  public function setResultData(AlpdeskCoreAuthResponse $resultData) {
-    $this->resultData = $resultData;
-  }
-
+    public function setResultData(AlpdeskCoreAuthResponse $resultData)
+    {
+        $this->resultData = $resultData;
+    }
 }
