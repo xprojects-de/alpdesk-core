03bd14ffad297963316825a67770b13feaca77cc
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 15:12:59 +0100

    code refactoring

--- src/Events/Event/AlpdeskCoreMandantListEvent.php
+++ src/Events/Event/AlpdeskCoreMandantListEvent.php
@@ -7,22 +7,24 @@ namespace Alpdesk\AlpdeskCore\Events\Event;
 use Symfony\Contracts\EventDispatcher\Event;
 use Alpdesk\AlpdeskCore\Library\Mandant\AlpdeskCoreMandantResponse;
 
-class AlpdeskCoreMandantListEvent extends Event {
+class AlpdeskCoreMandantListEvent extends Event
+{
+    public const NAME = 'alpdesk.mandantlist';
 
-  public const NAME = 'alpdesk.mandantlist';
+    private AlpdeskCoreMandantResponse $resultData;
 
-  private AlpdeskCoreMandantResponse $resultData;
+    public function __construct(AlpdeskCoreMandantResponse $resultData)
+    {
+        $this->resultData = $resultData;
+    }
 
-  public function __construct(AlpdeskCoreMandantResponse $resultData) {
-    $this->resultData = $resultData;
-  }
+    public function getResultData(): AlpdeskCoreMandantResponse
+    {
+        return $this->resultData;
+    }
 
-  public function getResultData(): AlpdeskCoreMandantResponse {
-    return $this->resultData;
-  }
-
-  public function setResultData(AlpdeskCoreMandantResponse $resultData) {
-    $this->resultData = $resultData;
-  }
-
+    public function setResultData(AlpdeskCoreMandantResponse $resultData)
+    {
+        $this->resultData = $resultData;
+    }
 }
