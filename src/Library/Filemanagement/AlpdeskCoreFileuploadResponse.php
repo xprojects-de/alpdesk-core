650cd046e0f2f55aece76c466f3aefb1dc8ece78
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 15:53:30 +0100

    code refactoring

--- src/Library/Filemanagement/AlpdeskCoreFileuploadResponse.php
+++ src/Library/Filemanagement/AlpdeskCoreFileuploadResponse.php
@@ -6,70 +6,83 @@ namespace Alpdesk\AlpdeskCore\Library\Filemanagement;
 
 use Alpdesk\AlpdeskCore\Library\Mandant\AlpdescCoreBaseMandantInfo;
 
-class AlpdeskCoreFileuploadResponse {
+class AlpdeskCoreFileuploadResponse
+{
+    private string $alpdesk_token = '';
+    private string $username = '';
+    private AlpdescCoreBaseMandantInfo $mandantInfo;
+    private string $fileName = '';
+    private string $rootFileName = '';
+    private array $data = array();
+    private string $uuid = '';
 
-  private string $alpdesk_token = '';
-  private string $username = '';
-  private AlpdescCoreBaseMandantInfo $mandantInfo;
-  private string $fileName = '';
-  private string $rootFileName = '';
-  private array $data = array();
-  private string $uuid = '';
+    public function getAlpdesk_token(): string
+    {
+        return $this->alpdesk_token;
+    }
 
-  public function getAlpdesk_token(): string {
-    return $this->alpdesk_token;
-  }
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
 
-  public function getUsername(): string {
-    return $this->username;
-  }
+    public function setAlpdesk_token(string $alpdesk_token): void
+    {
+        $this->alpdesk_token = $alpdesk_token;
+    }
 
-  public function setAlpdesk_token(string $alpdesk_token): void {
-    $this->alpdesk_token = $alpdesk_token;
-  }
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
 
-  public function setUsername(string $username): void {
-    $this->username = $username;
-  }
+    public function getData(): array
+    {
+        return $this->data;
+    }
 
-  public function getData(): array {
-    return $this->data;
-  }
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
 
-  public function setData(array $data): void {
-    $this->data = $data;
-  }
+    public function getMandantInfo(): AlpdescCoreBaseMandantInfo
+    {
+        return $this->mandantInfo;
+    }
 
-  public function getMandantInfo(): AlpdescCoreBaseMandantInfo {
-    return $this->mandantInfo;
-  }
+    public function setMandantInfo(AlpdescCoreBaseMandantInfo $mandantInfo): void
+    {
+        $this->mandantInfo = $mandantInfo;
+    }
 
-  public function setMandantInfo(AlpdescCoreBaseMandantInfo $mandantInfo): void {
-    $this->mandantInfo = $mandantInfo;
-  }
+    public function getFileName(): string
+    {
+        return $this->fileName;
+    }
 
-  public function getFileName(): string {
-    return $this->fileName;
-  }
+    public function setFileName(string $fileName): void
+    {
+        $this->fileName = $fileName;
+    }
 
-  public function setFileName(string $fileName): void {
-    $this->fileName = $fileName;
-  }
+    public function getRootFileName(): string
+    {
+        return $this->rootFileName;
+    }
 
-  public function getRootFileName(): string {
-    return $this->rootFileName;
-  }
+    public function setRootFileName(string $rootFileName): void
+    {
+        $this->rootFileName = $rootFileName;
+    }
 
-  public function setRootFileName(string $rootFileName): void {
-    $this->rootFileName = $rootFileName;
-  }
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
 
-  public function getUuid(): string {
-    return $this->uuid;
-  }
-
-  public function setUuid(string $uuid): void {
-    $this->uuid = $uuid;
-  }
-
+    public function setUuid(string $uuid): void
+    {
+        $this->uuid = $uuid;
+    }
 }
