9de871c5cfd3a3ed34e1c757a186c1c3af551a40
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 16:20:34 +0100

    code refactoring

--- src/Security/AlpdeskcoreInputSecurity.php
+++ src/Security/AlpdeskcoreInputSecurity.php
@@ -7,41 +7,68 @@ namespace Alpdesk\AlpdeskCore\Security;
 use Contao\Input;
 use Contao\Config;
 
-class AlpdeskcoreInputSecurity {
+class AlpdeskcoreInputSecurity
+{
+    /**
+     * @param $varValue
+     * @param false $blnDecodeEntities
+     * @return array|mixed|null
+     * @throws \Exception
+     */
+    public static function secureValue($varValue, $blnDecodeEntities = false)
+    {
+        if ($varValue === null) {
+            throw new \Exception('value is null at secureValue');
+        }
 
-  public static function secureValue($varValue, $blnDecodeEntities = false) {
-    if ($varValue === null) {
-      throw new \Exception('value is null at secureValue');
-    }
-    $varValue = Input::decodeEntities($varValue);
-    $varValue = Input::xssClean($varValue, true);
-    $varValue = Input::stripTags($varValue);
-    if (!$blnDecodeEntities) {
-      $varValue = Input::encodeSpecialChars($varValue);
-    }
-    return $varValue;
-  }
+        $varValue = Input::decodeEntities($varValue);
+        $varValue = Input::xssClean($varValue, true);
+        $varValue = Input::stripTags($varValue);
 
-  public static function secureHtmlValue($varValue, $blnDecodeEntities = false) {
-    if ($varValue === null) {
-      throw new \Exception('value is null at secureValue');
-    }
-    $varValue = Input::decodeEntities($varValue);
-    $varValue = Input::xssClean($varValue);
-    $varValue = Input::stripTags($varValue, Config::get('allowedTags'));
-    if (!$blnDecodeEntities) {
-      $varValue = Input::encodeSpecialChars($varValue);
-    }
-    return $varValue;
-  }
+        if (!$blnDecodeEntities) {
+            $varValue = Input::encodeSpecialChars($varValue);
+        }
 
-  public static function secureRawValue($varValue) {
-    if ($varValue === null) {
-      throw new \Exception('value is null at secureValue');
+        return $varValue;
     }
-    $varValue = Input::preserveBasicEntities($varValue);
-    $varValue = Input::xssClean($varValue);
-    return $varValue;
-  }
 
+    /**
+     * @param $varValue
+     * @param false $blnDecodeEntities
+     * @return array|mixed|null
+     * @throws \Exception
+     */
+    public static function secureHtmlValue($varValue, $blnDecodeEntities = false)
+    {
+        if ($varValue === null) {
+            throw new \Exception('value is null at secureValue');
+        }
+
+        $varValue = Input::decodeEntities($varValue);
+        $varValue = Input::xssClean($varValue);
+        $varValue = Input::stripTags($varValue, Config::get('allowedTags'));
+
+        if (!$blnDecodeEntities) {
+            $varValue = Input::encodeSpecialChars($varValue);
+        }
+
+        return $varValue;
+    }
+
+    /**
+     * @param $varValue
+     * @return array|bool|int|mixed|string|null
+     * @throws \Exception
+     */
+    public static function secureRawValue($varValue)
+    {
+        if ($varValue === null) {
+            throw new \Exception('value is null at secureValue');
+        }
+
+        $varValue = Input::preserveBasicEntities($varValue);
+        $varValue = Input::xssClean($varValue);
+
+        return $varValue;
+    }
 }
