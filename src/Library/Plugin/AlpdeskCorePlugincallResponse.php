f7156f7179d54768809c6827ab8116fcd3d09036
XProjects B.Hummel <infos@x-projects.de>
2021-03-22 16:04:12 +0100

    code refactoring

--- src/Library/Plugin/AlpdeskCorePlugincallResponse.php
+++ src/Library/Plugin/AlpdeskCorePlugincallResponse.php
@@ -6,61 +6,72 @@ namespace Alpdesk\AlpdeskCore\Library\Plugin;
 
 use Alpdesk\AlpdeskCore\Library\Mandant\AlpdescCoreBaseMandantInfo;
 
-class AlpdeskCorePlugincallResponse {
+class AlpdeskCorePlugincallResponse
+{
+    private string $alpdesk_token = '';
+    private string $username = '';
+    private string $plugin = '';
+    private AlpdescCoreBaseMandantInfo $mandantInfo;
+    private array $requestData = array();
+    private array $data = array();
 
-  private string $alpdesk_token = '';
-  private string $username = '';
-  private string $plugin = '';
-  private AlpdescCoreBaseMandantInfo $mandantInfo;
-  private array $requestData = array();
-  private array $data = array();
+    public function getAlpdesk_token(): string
+    {
+        return $this->alpdesk_token;
+    }
 
-  public function getAlpdesk_token(): string {
-    return $this->alpdesk_token;
-  }
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
 
-  public function getUsername(): string {
-    return $this->username;
-  }
+    public function setAlpdesk_token(string $alpdesk_token): void
+    {
+        $this->alpdesk_token = $alpdesk_token;
+    }
 
-  public function setAlpdesk_token(string $alpdesk_token): void {
-    $this->alpdesk_token = $alpdesk_token;
-  }
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
 
-  public function setUsername(string $username): void {
-    $this->username = $username;
-  }
+    public function getPlugin(): string
+    {
+        return $this->plugin;
+    }
 
-  public function getPlugin(): string {
-    return $this->plugin;
-  }
+    public function getData(): array
+    {
+        return $this->data;
+    }
 
-  public function getData(): array {
-    return $this->data;
-  }
+    public function setPlugin(string $plugin): void
+    {
+        $this->plugin = $plugin;
+    }
 
-  public function setPlugin(string $plugin): void {
-    $this->plugin = $plugin;
-  }
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
 
-  public function setData(array $data): void {
-    $this->data = $data;
-  }
+    public function getMandantInfo(): AlpdescCoreBaseMandantInfo
+    {
+        return $this->mandantInfo;
+    }
 
-  public function getMandantInfo(): AlpdescCoreBaseMandantInfo {
-    return $this->mandantInfo;
-  }
+    public function setMandantInfo(AlpdescCoreBaseMandantInfo $mandantInfo): void
+    {
+        $this->mandantInfo = $mandantInfo;
+    }
 
-  public function setMandantInfo(AlpdescCoreBaseMandantInfo $mandantInfo): void {
-    $this->mandantInfo = $mandantInfo;
-  }
+    public function getRequestData(): array
+    {
+        return $this->requestData;
+    }
 
-  public function getRequestData(): array {
-    return $this->requestData;
-  }
-
-  public function setRequestData(array $requestData): void {
-    $this->requestData = $requestData;
-  }
-
+    public function setRequestData(array $requestData): void
+    {
+        $this->requestData = $requestData;
+    }
 }
