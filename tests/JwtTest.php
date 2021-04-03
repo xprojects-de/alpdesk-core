0efd21a0924d45cdc2ca2f0f9081f1415807b3d8
Benjamin Hummel <55982921+xprojects-de@users.noreply.github.com>
2021-04-03 10:56:25 +0200

    adding PHpUnit

---
+++ tests/JwtTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alpdesk\AlpdeskCore\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class JwtTest extends TestCase
+{
+
+    public function testJwt()
+    {
+        // For Future tests
+
+        $this->assertTrue(true);
+    }
+
+}
