89b76f3a4ba2b1c92d4060d99126ea7a4ac59761
Benjamin Hummel <55982921+xprojects-de@users.noreply.github.com>
2021-04-10 07:44:49 +0200

    Logs working

--- src/Controller/Logs/AlpdeskcoreLogsController.php
+++ src/Controller/Logs/AlpdeskcoreLogsController.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Alpdesk\AlpdeskCore\Controller\Logs;
 
+use Contao\Controller;
+use Contao\File;
+use Contao\Input;
+use Contao\System;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,14 +25,72 @@ class AlpdeskcoreLogsController extends AbstractController
     private $csrfTokenName;
     protected $router;
     private $security;
+    private $projectDir;
 
-    public function __construct(TwigEnvironment $twig, CsrfTokenManagerInterface $csrfTokenManager, string $csrfTokenName, RouterInterface $router, Security $security)
+    public function __construct(TwigEnvironment $twig, CsrfTokenManagerInterface $csrfTokenManager, string $csrfTokenName, RouterInterface $router, Security $security, string $projectDir)
     {
         $this->twig = $twig;
         $this->csrfTokenManager = $csrfTokenManager;
         $this->csrfTokenName = $csrfTokenName;
         $this->router = $router;
         $this->security = $security;
+        $this->projectDir = $projectDir;
+    }
+
+    private static function startsWith(string $startString, string $string): bool
+    {
+        $len = \strlen($startString);
+        $sub = \substr($string, 0, $len);
+
+        return ($sub === $startString);
+    }
+
+    private function scanDir(string $parseFolder): array
+    {
+        $strFolder = $this->projectDir . '/' . $parseFolder;
+
+        $arrReturn = [];
+
+        foreach (\scandir($strFolder, SCANDIR_SORT_ASCENDING) as $strFile) {
+
+            if ($strFile == '.' || $strFile == '..' || self::startsWith('.', $strFile)) {
+                continue;
+            }
+
+            $logFile = new File($parseFolder . '/' . $strFile);
+            if ($logFile->exists() && $logFile->extension === 'log') {
+
+                $content = $logFile->getContentAsArray();
+
+                $arrReturn[] = [
+                    'logfile' => $logFile->name,
+                    'content' => $content
+                ];
+
+            }
+        }
+
+        \usort($arrReturn, function ($a, $b) {
+            return $b['logfile'] <=> $a['logfile'];
+        });
+
+        return $arrReturn;
+    }
+
+    private function deleteLog(string $parseFolder)
+    {
+        $deleteLog = Input::get('deleteLog');
+        if ($deleteLog !== null && $deleteLog !== '') {
+
+            $logFile = new File($parseFolder . '/' . $deleteLog);
+            if ($logFile->exists()) {
+                $logFile->delete();
+            }
+
+            Controller::redirect($this->router->generate('alpdesk_logs_backend'));
+
+        }
+
     }
 
     public function endpoint(): Response
@@ -36,10 +98,19 @@ class AlpdeskcoreLogsController extends AbstractController
         $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/alpdeskcore/js/alpdeskcore_logs.js';
         $GLOBALS['TL_CSS'][] = 'bundles/alpdeskcore/css/alpdeskcore_logs.css';
 
+        System::loadLanguageFile('default');
+
+        $this->deleteLog('var/logs');
+        $logFiles = $this->scanDir('var/logs');
+
+        $numberOfLogs = \count($logFiles);
+
         $outputTwig = $this->twig->render('@AlpdeskCore/alpdeskcorelogs_be.html.twig', [
             'token' => $this->csrfTokenManager->getToken($this->csrfTokenName)->getValue(),
-            'base' => Environment::get('base'),
-            'msg' => 'Hallo Welt'
+            'route' => $this->router->generate('alpdesk_logs_backend'),
+            'confirm' => $GLOBALS['TL_LANG']['MOD']['alpdeskcorelogs_confirm'],
+            'numberOfLogs' => $GLOBALS['TL_LANG']['MOD']['alpdeskcorelogs_logcount'] . ': ' . $numberOfLogs,
+            'logs' => $logFiles
         ]);
 
         return new Response($outputTwig);
