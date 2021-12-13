afb9221fe73067420113d23d3aef2a33409f8128
Benjamin Hummel <55982921+xprojects-de@users.noreply.github.com>
2021-12-13 20:27:55 +0100

    code cleanup and bugfixes

--- src/Jwt/JwtToken.php
+++ src/Jwt/JwtToken.php
@@ -19,25 +19,37 @@ class JwtToken
     private static string $issuedBy = 'Alpdesk';
     private static string $permittedFor = 'https://alpdesk.de';
 
+    /**
+     * @return string
+     */
     private static function getDefaultKeyString(): string
     {
         $keyString = System::getContainer()->getParameter('kernel.secret');
         return substr($keyString, 10, 32);
     }
 
+    /**
+     * @return Configuration
+     */
     private static function getConfig(): Configuration
     {
         return Configuration::forSymmetricSigner(new Sha256(), InMemory::plainText(self::getDefaultKeyString()));
     }
 
+    /**
+     * @param string $jti
+     * @param int $nbf
+     * @param array $claims
+     * @return string
+     */
     public static function generate(string $jti, int $nbf = 3600, array $claims = array()): string
     {
-        $time = time();
+        $time = \time();
 
         $config = self::getConfig();
 
         $issuesAt = (new \DateTimeImmutable())->setTimestamp($time);
-        $usedAfter = (new \DateTimeImmutable())->setTimestamp($time + 0);
+        $usedAfter = (new \DateTimeImmutable())->setTimestamp($time);
         $expiresAt = (new \DateTimeImmutable())->setTimestamp($time + $nbf);
 
         $builder = $config->builder();
@@ -62,6 +74,10 @@ class JwtToken
         return $token->toString();
     }
 
+    /**
+     * @param string $token
+     * @return Token
+     */
     public static function parse(string $token): Token
     {
         $config = self::getConfig();
@@ -70,6 +86,11 @@ class JwtToken
         return $parser->parse($token);
     }
 
+    /**
+     * @param string $token
+     * @param string $name
+     * @return mixed|null
+     */
     public static function getClaim(string $token, string $name)
     {
         try {
@@ -84,6 +105,11 @@ class JwtToken
         return $value;
     }
 
+    /**
+     * @param string $token
+     * @param string $jti
+     * @return bool
+     */
     public static function validateAndVerify(string $token, string $jti): bool
     {
         $tokenObject = self::parse($token);
