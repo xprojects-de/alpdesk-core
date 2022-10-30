07a9052f90654b5e5dac5abe5f240c9b729aa6de
Benjamin Hummel <55982921+xprojects-de@users.noreply.github.com>
2022-10-30 18:58:58 +0100

    bugfixes php8 warnings

--- src/Resources/contao/dca/tl_alpdeskcore_sessions.php
+++ src/Resources/contao/dca/tl_alpdeskcore_sessions.php
@@ -53,7 +53,7 @@ $GLOBALS['TL_DCA']['tl_alpdeskcore_sessions'] = array
                 'label' => &$GLOBALS['TL_LANG']['tl_alpdeskcore_sessions']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes' => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             )
         )
     ),
