cde9a3f070ab8b836530870d763a1ca33409d2b5
Benjamin Hummel <infos@x-projects.de>
2025-11-07 15:32:32 +0100

    bugfixes

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['MOD']['alpdeskcore'] = array('Alpdesk-Core', '');
+$GLOBALS['TL_LANG']['MOD']['alpdeskcore'] = array('Alpdesk', '');
 $GLOBALS['TL_LANG']['MOD']['alpdeskcore_sessions'] = array('Benutzer-Sessions', '');
 $GLOBALS['TL_LANG']['MOD']['alpdeskcore_mandant'] = array('Mandanten', '');
 $GLOBALS['TL_LANG']['MOD']['alpdeskcore_databasemanager'] = array('Datenbank-Manager', '');
